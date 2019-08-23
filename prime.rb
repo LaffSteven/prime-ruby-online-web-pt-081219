f085424f2e1a38ef28d3c0d6ec8b709683046708
Steven Laff <steve.laff@gmail.com>
2019-08-23 03:15:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  dividers = (2..num).to_a
+  if num == 2
+    return true
+  else
+
+    dividers.each do |d|
+      puts (num / d).to_f
+      if (num / d).to_f % 1 == 0
+        return false
+      end
+    end
+  end
+end
